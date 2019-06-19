518e58a0d9d002e946b4f914cdfb29cd5daff50d
Giulio Roggero <giulio.roggero@mia-platform.eu>
2019-06-19 10:02:38 +0200

    habits

--- src/create_habits.sh
+++ src/create_habits.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-FNAME=habits.java
-echo "//TODO" > $FNAME
-for i in {1..7}
+DNAME=habit
+SEV_HABITS=( 'autorepair' 'informagics' 'balance_tech_debt-time_to_market-tco' 'all_as_code' 'good_design' 'pareto' 'evolutionary_design' )
+for hab in "${SEV_HABITS[@]}"
 do
-  cp $FNAME ${i}_$FNAME
+  DIR=${hab}_$DNAME
+  echo "Making dir ${DIR}"
+  mkdir ${DIR}
+  cd ${DIR}
+  echo "# {hab}" > README.md
+  cd ..
 done
-rm $FNAME
